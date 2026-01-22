4c6b078f932d5f06f95ab00d475c7ca5945de3b4
Connor Russell <116117483+Connor-Russell@users.noreply.github.com>
2026-01-22 15:25:16 -0800

    Fixed error when exporting agp with normal attached objects

--- io_scene_xplane_ext/Types/xp_agp.py
+++ io_scene_xplane_ext/Types/xp_agp.py
@@ -368,7 +368,7 @@ class attached_obj:
 
     def from_obj(self, obj):
 
-        attached_obj_file_name = os.path.basename(obj.xp_agp.resource)
+        attached_obj_file_name = os.path.basename(obj.xp_agp.attached_obj_resource)
         attached_obj_preview_file_name = os.path.basename(file_utils.to_absolute(obj.xp_agp.attached_obj_preview_resource))
 
         if not attached_obj_file_name == attached_obj_preview_file_name and attached_obj_preview_file_name != "":
